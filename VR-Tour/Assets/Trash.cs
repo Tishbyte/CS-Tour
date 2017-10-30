ecdeea5efba8874a5311cb80c28a2ccfd1f45a26
Tishbyte <tishbyte@outlook.com>
2017-10-30 16:31:36 -0700

    Added bin.

---
+++ VR-Tour/Assets/Trash.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Trash : MonoBehaviour {
+
+    public Text txt;
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Trash")
+        {
+            Destroy(collision.gameObject);
+            txt.gameObject.GetComponent<Text>().text = (int.Parse(txt.gameObject.GetComponent<Text>().text) + 1).ToString();
+        }
+    }
+}
