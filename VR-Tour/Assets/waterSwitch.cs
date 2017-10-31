9b424323c9ebdc682cbb1b65adbff1a02179dadc
Tishbyte <tishbyte@outlook.com>
2017-10-31 16:42:11 -0700

    Added water fountain.

---
+++ VR-Tour/Assets/waterSwitch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class waterSwitch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<ParticleSystem>().Stop();
+    }
+    
+
+
+    // Update is called once per frame
+    public void toggleWater()
+    {
+        if (gameObject.GetComponent<ParticleSystem>().isPlaying == true)
+        {
+            gameObject.GetComponent<ParticleSystem>().Stop();
+        }
+        else
+        {
+            gameObject.GetComponent<ParticleSystem>().Play();
+        }
+    }
+}
