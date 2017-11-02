0fe6959edd9ac7227ea8f959ce3b3a93c0094cb1
Tishbyte <tishbyte@outlook.com>
2017-11-02 16:55:31 -0700

    Fixed some bugs and started adding the dispenser.

---
+++ VR-Tour/Assets/dispenseDrink.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dispenseDrink : MonoBehaviour {
+
+    public GameObject prefab;
+
+    public void spawnDrink()
+    {
+        Instantiate(prefab);
+    }
+}
---
+++ VR-Tour/Assets/drinkExplode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drinkExplode : MonoBehaviour {
+
+    float v3Velocity;
+    int counter = 0;
+
+    // Update is called once per frame
+    void Update () {
+        v3Velocity = gameObject.GetComponent<Rigidbody>().velocity.magnitude;
+        if (v3Velocity > 1)
+        {
+            Debug.Log(counter + "/" +v3Velocity);
+            counter++;
+            if (counter > 10)
+            {
+                Destroy(gameObject);
+            }
+        }
+	}
+}
