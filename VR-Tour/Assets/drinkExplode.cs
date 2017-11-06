871947a9c56b67c697f500632bd231b70ce29d8b
Tishbyte <tishbyte@outlook.com>
2017-11-06 13:56:28 -0800

    Testing with particle systems.

--- VR-Tour/Assets/drinkExplode.cs
+++ VR-Tour/Assets/drinkExplode.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using VRTK;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,18 +7,28 @@ public class drinkExplode : MonoBehaviour {
 
     float v3Velocity;
     int counter = 0;
-
-    // Update is called once per frame
-    void Update () {
-        v3Velocity = gameObject.GetComponent<Rigidbody>().velocity.magnitude;
-        if (v3Velocity > 1)
+    void Start()
+    {
+        gameObject.GetComponent<ParticleSystem>().Stop();
+        //make sure the object has the VRTK script attached... 
+        if (GetComponent<VRTK_InteractableObject>() == null)
         {
-            Debug.Log(counter + "/" +v3Velocity);
-            counter++;
-            if (counter > 10)
-            {
-                Destroy(gameObject);
-            }
+            Debug.LogError("Error.");
+            return;
         }
-	}
+        //subscribe to the event.  NOTE: the "ObectGrabbed"  this is the procedure to invoke if this objectis grabbed.. 
+        GetComponent<VRTK_InteractableObject>().InteractableObjectUsed += new InteractableObjectEventHandler(explode);
+    }
+
+    private void explode(object sender, InteractableObjectEventArgs e)
+    {
+        counter++;
+        gameObject.GetComponent<ParticleSystem>().Play();
+        gameObject.GetComponent<ParticleSystem>().Stop();
+        if (counter > 5)
+        {
+            gameObject.GetComponent<ParticleSystem>().Play();
+            Destroy(gameObject);
+        }
+    }
 }
