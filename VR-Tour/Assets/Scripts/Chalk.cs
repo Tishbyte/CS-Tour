c85ca1860770a4cfc0756529135cb25c7805a9b7
Tishbyte <tishbyte@outlook.com>
2017-11-27 16:20:41 -0800

    Got the basics of the chalkboard/whiteboard.

---
+++ VR-Tour/Assets/Scripts/Chalk.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Chalk : VRTK_InteractableObject
+{
+
+    private VRTK_ControllerActions controllerActions;
+    public ChalkBoard chalkboard;
+    private RaycastHit touch;
+    private Quaternion lastAngle;
+    private bool lastTouch;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Get our Whiteboard component from the whiteboard object
+        this.chalkboard = GameObject.Find("ChalkBoard").GetComponent<ChalkBoard>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float tipHeight = transform.Find("Tip").transform.localScale.y;
+        Vector3 tip = transform.Find("Tip").transform.position;
+
+        //Debug.Log(tip);
+
+        if (lastTouch)
+        {
+            tipHeight *= 1.1f;
+        }
+
+        // Check for a Raycast from the tip of the pen
+        if (Physics.Raycast(tip, transform.up, out touch, tipHeight))
+        {
+            if (!(touch.collider.tag == "ChalkBoard")) return;
+            this.chalkboard = touch.collider.GetComponent<ChalkBoard>();
+
+            // Give haptic feedback when touching the whiteboard
+            controllerActions.TriggerHapticPulse(0.05f);
+
+            // Set whiteboard parameters
+            chalkboard.SetColor(Color.white);
+            chalkboard.SetTouchPosition(touch.textureCoord.x, touch.textureCoord.y);
+            chalkboard.ToggleTouch(true);
+
+            // If we started touching, get the current angle of the pen
+            if (lastTouch == false)
+            {
+                lastTouch = true;
+                lastAngle = transform.rotation;
+            }
+        }
+        else
+        {
+            chalkboard.ToggleTouch(false);
+            lastTouch = false;
+        }
+
+        // Lock the rotation of the pen if "touching"
+        if (lastTouch)
+        {
+            transform.rotation = lastAngle;
+        }
+    }
+
+    public override void Grabbed(VRTK_InteractGrab grabbingObject)
+    {
+        base.Grabbed(grabbingObject);
+        controllerActions = grabbingObject.GetComponent<VRTK_ControllerActions>();
+    }
+}
---
+++ VR-Tour/Assets/Scripts/ChalkBoard.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ChalkBoard : MonoBehaviour
+{
+
+    private int textureSize = 2048;
+    private int chalkSize = 10;
+    private Texture2D texture;
+    private Color[] color;
+
+    private bool touching, touchingLast;
+    private float posX, posY;
+    private float lastX, lastY;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Set whiteboard texture
+        Renderer renderer = GetComponent<Renderer>();
+        this.texture = new Texture2D(textureSize, textureSize);
+        renderer.material.mainTexture = (Texture)texture;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Transform textureCoords into "pixel" values
+        int x = (int)(posX * textureSize - (chalkSize / 2));
+        int y = (int)(posY * textureSize - (chalkSize / 2));
+
+        // Only set the pixels if we were touching last frame
+        if (touchingLast)
+        {
+            // Set base touch pixels
+            texture.SetPixels(x, y, chalkSize, chalkSize, color);
+
+            // Interpolate pixels from previous touch
+            for (float t = 0.01f; t < 1.00f; t += 0.01f)
+            {
+                int lerpX = (int)Mathf.Lerp(lastX, (float)x, t);
+                int lerpY = (int)Mathf.Lerp(lastY, (float)y, t);
+                texture.SetPixels(lerpX, lerpY, chalkSize, chalkSize, color);
+            }
+        }
+
+        // If currently touching, apply the texture
+        if (touching)
+        {
+            texture.Apply();
+        }
+
+        this.lastX = (float)x;
+        this.lastY = (float)y;
+
+        this.touchingLast = this.touching;
+    }
+
+    public void ToggleTouch(bool touching)
+    {
+        this.touching = touching;
+    }
+
+    public void SetTouchPosition(float x, float y)
+    {
+        this.posX = x;
+        this.posY = y;
+    }
+
+    public void SetColor(Color color)
+    {
+        this.color = Enumerable.Repeat<Color>(color, chalkSize * chalkSize).ToArray<Color>();
+    }
+}
