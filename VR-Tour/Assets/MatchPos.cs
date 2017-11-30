7d36cd055e4710b0f3408c3b84431d4c0be707e1
Tishbyte <tishbyte@outlook.com>
2017-11-29 16:04:41 -0800

    Added basic headset and controller configuration for the models.

---
+++ VR-Tour/Assets/MatchPos.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchPos : MonoBehaviour {
+
+    public GameObject htcVive;
+
+	// Update is called once per frame
+	void Update () {
+        htcVive.transform.position = new Vector3(this.transform.localPosition.x + 11.5f, this.transform.localPosition.y + 26, this.transform.localPosition.z);
+        htcVive.transform.localRotation = this.transform.localRotation;
+    }
+}
