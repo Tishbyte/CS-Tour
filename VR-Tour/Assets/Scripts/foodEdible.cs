4dd258b285093d6acd15fb498fb59d79ac4a20d2
DVDBZN <dbozin60@bigbend.edu>
2017-12-29 17:25:09 -0800

    Did testing with performance tuning, added four edible foods and their remains for those applicable, tagged more items as trash, and found that adjusting the height might be a fruitless effort.

---
+++ VR-Tour/Assets/Scripts/foodEdible.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class foodEdible : MonoBehaviour {
+
+    public MeshFilter nonEdible;
+    public bool leftover;
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (leftover == true)
+            {
+                this.gameObject.GetComponent<MeshFilter>().mesh = nonEdible.sharedMesh;
+                this.gameObject.GetComponent<MeshCollider>().sharedMesh = nonEdible.sharedMesh;
+                this.gameObject.GetComponent<foodEdible>().enabled = false;
+            }
+            else
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
