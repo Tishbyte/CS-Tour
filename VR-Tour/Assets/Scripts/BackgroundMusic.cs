8fcbc552b6f6056575042d71b862ca6896eca3c2
Tishbyte <tishbyte@outlook.com>
2018-01-22 09:41:32 -0800

    Added background music.

---
+++ VR-Tour/Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour {
+
+    public AudioClip [] music;
+    private int counter = 0;
+
+    void Start()
+    {
+        this.GetComponent<AudioSource>().clip = music[counter];
+        this.GetComponent<AudioSource>().Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (this.GetComponent<AudioSource>().isPlaying == false)
+        {
+            if (counter == 2)
+            {
+                counter = 0;
+            }
+            else
+            {
+                counter++;
+            }
+
+            this.GetComponent<AudioSource>().clip = music[counter];
+            this.GetComponent<AudioSource>().Play();
+        }
+	}
+}
