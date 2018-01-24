c8609805b4136aa922153ef56bfc3f7b62c637f5
Tishbyte <tishbyte@outlook.com>
2018-01-24 11:21:14 -0800

    Added viking helmet, tweeked the mirrors, and tweeked quality settings.

---
+++ VR-Tour/Assets/Scripts/food.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class food : MonoBehaviour {
+	// Update is called once per frame
+	void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
