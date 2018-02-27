b9586bc894d8b3d64a30d0fc4f4455c6507b9caa
Tishbyte <tishbyte@outlook.com>
2018-02-27 13:16:02 -0800

    Added controller tooltips.

---
+++ VR-Tour/Assets/TooltipDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TooltipDisplay : MonoBehaviour {
+
+    public GameObject face;
+	// Update is called once per frame
+	void Update () {
+        if (Vector3.Distance(face.transform.position, transform.position) > .3)
+        {
+            this.gameObject.transform.GetChild(0).gameObject.active = false;
+            this.gameObject.transform.GetChild(1).gameObject.active = false;
+            this.gameObject.transform.GetChild(2).gameObject.active = false;
+        }
+        else
+        {
+            this.gameObject.transform.GetChild(0).gameObject.active = true;
+            this.gameObject.transform.GetChild(1).gameObject.active = true;
+            this.gameObject.transform.GetChild(2).gameObject.active = true;
+        }
+	}
+}
